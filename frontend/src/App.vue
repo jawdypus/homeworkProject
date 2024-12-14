ce971ab35e9060e1925ae9b7621df37cecbc0198
jawdypus <auditoreven@gmail.com>
2024-12-15 00:34:29 +0200

    added vue to project

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import {ref} from 'vue';
+
+const homework = ref({});
+
+
+const fetchHomework = () => {
+  fetch('http://127.0.0.1:8080/get-homework')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    homework.value = data
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Parsing error:', error);
+  });
+};
+
+</script>
+
+<template>
+  <button @click="fetchHomework">Get homework</button>
+  <p>{{ homework }}</p>
+</template>
+
+
